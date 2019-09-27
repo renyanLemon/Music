5d8a0deaa91e85a130682b558354542a8d8b5c66
vvvvvic <39343739+vvvvvic@users.noreply.github.com>
2019-09-27 14:55:27 +0800

    feat: add iconfont and paly music

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -5,19 +5,25 @@ let songslist = []
 //当前正在播放歌曲的index
 let nowPlayingIndex = 0
 
+//获取全局唯一的背景音频管理器
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+
 Page({
 
   data: {
-    picUrl: ''
+    picUrl: '',
+    isPlaying: false
   },
 
   onLoad: function (options) {
+    console.log('options', options)
     songslist = wx.getStorageSync('songslist')
     nowPlayingIndex = options.index
-    this._loadSongsDetail()
+    this._loadSongsDetail(options.musicid)
   },
 
-  _loadSongsDetail() {
+  _loadSongsDetail(songId) {
+    backgroundAudioManager.stop()
     let songs = songslist[nowPlayingIndex]
     //将歌曲名称展示在tabbar
     wx.setNavigationBarTitle({
@@ -26,7 +32,59 @@ Page({
     this.setData({
       picUrl: songs.al.picUrl
     })
+    
+    wx.showLoading({
+      title: '加载中',
+    })
+
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        songId,
+        $url: 'songUrl',
+      }
+    }).then((res) => {
+      wx.hideLoading()
+      let result = JSON.parse(res.result)
+      backgroundAudioManager.src = result.data[0].url
+      backgroundAudioManager.title = songs.name
+      backgroundAudioManager.coverImgUrl = songs.al.picUrl
+      backgroundAudioManager.singer = songs.ar[0].name
+      backgroundAudioManager.epname = songs.al.name
+
+      this.setData({
+        isPlaying: true
+      })
+    })
+  },
+
+  togglePlaying() {
+    if(this.data.isPlaying) {
+      backgroundAudioManager.pause()
+    }else {
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+
+  onPrev() {
+    nowPlayingIndex--
+    if (nowPlayingIndex < 0) {
+      nowPlayingIndex = songslist.length -1
+    }
+    this._loadSongsDetail(songslist[nowPlayingIndex].id)
+  },
+
+  onNext() {
+    nowPlayingIndex++
+    if (nowPlayingIndex === songslist.length) {
+      nowPlayingIndex = 0
+    }
+    this._loadSongsDetail(songslist[nowPlayingIndex].id)
   }
+
 
   
 })
