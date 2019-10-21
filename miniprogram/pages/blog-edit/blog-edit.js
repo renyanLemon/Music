10785864b6ec4409b011ec966b83c7133d404db9
vvvvvic <39343739+vvvvvic@users.noreply.github.com>
2019-10-21 15:40:08 +0800

    feat: upload Image finish

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -2,6 +2,13 @@
 const MAX_WORDS_NUM = 140  //最大输入文字个数
 const MAX_IMG_NUM = 9   //最大上传图片数量
 
+//初始化数据库
+const db = wx.cloud.database()
+//输入的内容
+let content = ''
+//用户信息
+let userInfo = {}
+
 Page({
 
   data: {
@@ -10,8 +17,8 @@ Page({
     images: [],
   },
 
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    userInfo = options
   },
 
   //输入框
@@ -23,6 +30,8 @@ Page({
     this.setData({
       wordsNum
     })
+    //输入的内容
+    content = event.detail.value
   },
 
   //获取焦点
@@ -72,40 +81,71 @@ Page({
     })
   },
 
-  onReady: function () {
+  //发送
+  send() {
+    //输入的内容不能为空
+    if(content.trim() === '') {
+      wx.showModal({
+        title: '请输入内容',
+        content: '',
+      })
+      return
+    }
 
-  },
+    wx.showLoading({
+      title: '发布中',
+    })
 
-  onShow: function () {
+    let promiseArr = []
+    let fileIds = []
+    //图片上传
+    for(let i=0; i < this.data.images.length; i++) {
 
-  },
+      let p = new Promise((resolve, reject) => {
 
-  onHide: function () {
+        let item = this.data.images[i]
+        //文件扩展名
+        let suffix = /\.\w+$/.exec(item)[0]
+        wx.cloud.uploadFile({
+          cloudPath: `blog/${Date.now()}-${Math.random() * 10000}${suffix}`,
+          filePath: item,
+          success: (res) => {
+            fileIds = fileIds.concat(res.fileID)
+            resolve()
+          },
+          fail: (err) => {
+            console.log(err)
+            reject()
+          }
+        })
+      })
 
-  },
+      promiseArr.push(p)
+      
+    }
 
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //存入到云数据库   ...userInfo 扩展运算符  将userInfo的每个属性插入
+    Promise.all(promiseArr).then((res) => {
+      db.collection('blog').add({
+        data: {
+          ...userInfo,
+          content,
+          img: fileIds,
+          createTime: db.serverDate()
+        }
+      }).then((res) => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '发布成功',
+        })
+        //返回blog页面，并且刷新
+        wx.navigateBack()
+      })
+    }).catch((err) => {
+      wx.hideLoading()
+      wx.showToast({
+        title: '发布失败，请稍后重试',
+      })
+    })
   }
 })
