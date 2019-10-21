ce12c3df7178442b050972bca897c78ed86b618f
vvvvvic <39343739+vvvvvic@users.noreply.github.com>
2019-10-21 16:43:03 +0800

    feat: show blog-card finished

---
+++ cloudfunctions/blog/index.js
@@ -0,0 +1,32 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+//引入 TcbRouter
+const TcbRouter = require('tcb-router')
+
+//初始化数据库
+const db = cloud.database()
+
+const blogCollection = db.collection('blog')
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const app = new TcbRouter({
+    event
+  })
+
+  app.router('list', async (ctx, next) => {
+    let blogList = await blogCollection.skip(event.start)
+                  .limit(event.count)
+                  .orderBy('createTime', 'desc')
+                  .get()
+                  .then((res) => {
+                    return res.data
+                  })
+    ctx.body = blogList
+  })
+
+  return app.serve()
+}
